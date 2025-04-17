3f60db3b355c26deeb5f48cd55f5bb9e4d366811
SteveC <steve@stevecoast.com>
2025-04-16 22:22:15 -0700

    Initial commit: Rails app with Bootstrap and Google OAuth

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,15 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    user = User.from_omniauth(request.env["omniauth.auth"])
+    session[:user_id] = user.id
+    redirect_to root_path, notice: "Signed in successfully!"
+  end
+
+  def destroy
+    session[:user_id] = nil
+    redirect_to root_path, notice: "Signed out successfully!"
+  end
+end
---
+++ app/models/user.rb
@@ -0,0 +1,9 @@
+class User < ApplicationRecord
+  def self.from_omniauth(auth)
+    where(provider: auth.provider, uid: auth.uid).first_or_initialize.tap do |user|
+      user.name = auth.info.name
+      user.email = auth.info.email
+      user.save!
+    end
+  end
+end
